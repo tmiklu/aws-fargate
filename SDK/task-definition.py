18a6268a9f6c20432e2d5e8c2a7fcc9eb548a8fd
tmiklu <61796331+tmiklu@users.noreply.github.com>
2021-04-22 21:18:16 +0200

    Update task-definition.py

--- SDK/task-definition.py
+++ SDK/task-definition.py
@@ -1,46 +1,51 @@
-response = client.register_task_definition(
-    family='vls-dev',
-    taskRoleArn='arn:aws:iam::028960685088:role/ecsTaskExecutionRole',
-    executionRoleArn='arn:aws:iam::028960685088:role/ecsTaskExecutionRole',
-    networkMode='awsvpc',
-    containerDefinitions=[
-        {
-            'name': 'vls-dev',
-            'image': '028960685088.dkr.ecr.us-east-1.amazonaws.com/vls-repository:openjdk11-base',
-            'cpu': 0.25,
-            'memory': 512MB,
-            'memoryReservation': 256MB,
-            'portMappings': [
-                {
-                    'containerPort': 8081,
-                    'hostPort': 8081,
-                    'protocol': 'tcp'
-                },
-            ],
-            'essential': False,
-            'startTimeout': 10,
-            'stopTimeout': 10,
-            'user': 1001:1001,
-            'workingDirectory': '/home',
-            'disableNetworking': False,
-            'privileged': False,
-            'readonlyRootFilesystem': False,
+import boto3
 
-            'interactive': False,
-            'pseudoTerminal': False,
+client = boto3.client('ecs')
 
-        },
-    ],
-
-    requiresCompatibilities=[
-        'FARGATE',
-    ],
-    cpu=256,
-    memory=512,
-    tags=[
-        {
-            'key': 'env',
-            'value': 'dev'
-        },
-    ],
-)
+def create_task():
+    response = client.register_task_definition(
+        family='vls-dev',
+        taskRoleArn='arn:aws:iam::028960685088:role/ecsTaskExecutionRole',
+        executionRoleArn='arn:aws:iam::028960685088:role/ecsTaskExecutionRole',
+        networkMode='awsvpc',
+        containerDefinitions=[
+            {
+                'name': 'vls-dev',
+                'image': '028960685088.dkr.ecr.us-east-1.amazonaws.com/vls-repository:openjdk11-base',
+                'cpu': 256,
+                'memory': 512,
+                'memoryReservation': 256,
+                'portMappings': [
+                    {
+                        'containerPort': 8081,
+                        'hostPort': 8081,
+                        'protocol': 'tcp'
+                    },
+                ],
+                'essential': True,
+                'startTimeout': 10,
+                'stopTimeout': 10,
+                'user': '1001:1001',
+                'workingDirectory': '/home',
+                'disableNetworking': False,
+                'privileged': False,
+                'readonlyRootFilesystem': False,
+    
+                'interactive': False,
+                'pseudoTerminal': False,
+    
+            },
+        ],
+    
+        requiresCompatibilities=[
+            'FARGATE',
+        ],
+        cpu='256',
+        memory='512',
+        tags=[
+            {
+                'key': 'env',
+                'value': 'dev'
+            },
+        ],
+    )
