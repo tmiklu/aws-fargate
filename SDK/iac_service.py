79651ab77fb01f31be65a57409ef7de8a89df1ec
tmiklu <61796331+tmiklu@users.noreply.github.com>
2021-04-23 18:53:45 +0200

    Update iac_service.py

--- SDK/iac_service.py
+++ SDK/iac_service.py
@@ -1,33 +1,39 @@
 import boto3
 
 client = boto3.client('ecs')
+ssm    = boto3.client('ssm')
+
 
 def create_service():
-  response = client.create_service(
-      cluster='vls-demo-fargate',
-      serviceName='vls-dev',
-      taskDefinition='vls-dev',
-
-      desiredCount=1,
-      launchType='FARGATE',
-
-      platformVersion='1.4.0',
-
-      networkConfiguration={
-          'awsvpcConfiguration': {
-              'subnets': [
-                  'string',
-              ],
-              'securityGroups': [
-                  'string',
-              ],
-              'assignPublicIp': 'DISABLED'
-          }
-      },
-      tags=[
-          {
-              'key': 'env',
-              'value': 'dev'
-          },
-      ]
-  )
+    response = ssm.get_parameters(
+            Names=[
+                'COMPONENT',
+            ],
+            WithDecryption=False
+        )
+    for item in response['Parameters'][0]['Value'].split(','):
+        response = client.create_service(
+            cluster='vls-demo-fargate',
+            serviceName=name,
+            taskDefinition=name,
+            
+            desiredCount=1,
+            launchType='FARGATE',
+            
+            platformVersion='1.4.0',
+            
+            networkConfiguration={
+                'awsvpcConfiguration': {
+                    'subnets': [
+                        'subnet-0d5ea679e732e4b50',
+                    ],
+                    'assignPublicIp': 'DISABLED'
+                }
+            },
+            tags=[
+                {
+                    'key': 'env',
+                    'value': 'dev'
+                },
+            ]
+        )
