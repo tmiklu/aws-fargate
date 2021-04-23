ee1eb12ed9f6e1f2833e3206b207d751ca45d4a3
tmiklu <61796331+tmiklu@users.noreply.github.com>
2021-04-23 18:53:20 +0200

    Update iac_task-definition.py

--- SDK/iac_task-definition.py
+++ SDK/iac_task-definition.py
@@ -1,51 +1,59 @@
 import boto3
 
 client = boto3.client('ecs')
+ssm    = boto3.client('ssm')
 
 def create_task():
-    response = client.register_task_definition(
-        family='vls-dev',
-        taskRoleArn='arn:aws:iam::028960685088:role/ecsTaskExecutionRole',
-        executionRoleArn='arn:aws:iam::028960685088:role/ecsTaskExecutionRole',
-        networkMode='awsvpc',
-        containerDefinitions=[
-            {
-                'name': 'vls-dev',
-                'image': '028960685088.dkr.ecr.us-east-1.amazonaws.com/vls-repository:openjdk11-base',
-                'cpu': 256,
-                'memory': 512,
-                'memoryReservation': 256,
-                'portMappings': [
-                    {
-                        'containerPort': 8081,
-                        'hostPort': 8081,
-                        'protocol': 'tcp'
-                    },
-                ],
-                'essential': True,
-                'startTimeout': 10,
-                'stopTimeout': 10,
-                'user': '1001:1001',
-                'workingDirectory': '/home',
-                'disableNetworking': False,
-                'privileged': False,
-                'readonlyRootFilesystem': False,
-    
-                'interactive': False,
-                'pseudoTerminal': False,
-    
-            },
-        ],
-    
-        requiresCompatibilities=[
-            'FARGATE',
-        ],
-        cpu='256',
-        memory='512',
-        tags=[
-            {
-                'key': 'env',
-                'value': 'dev'
-            },
-        ],
-    )
+    response = ssm.get_parameters(
+            Names=[
+                'COMPONENT',
+            ],
+            WithDecryption=False
+        )
+    for name in response['Parameters'][0]['Value'].split(','):
+        response = client.register_task_definition(
+            family=name,
+            taskRoleArn='arn:aws:iam::028960685088:role/ecsTaskExecutionRole',
+            executionRoleArn='arn:aws:iam::028960685088:role/ecsTaskExecutionRole',
+            networkMode='awsvpc',
+            containerDefinitions=[
+                {
+                    'name': 'vls-dev',
+                    'image': '028960685088.dkr.ecr.us-east-1.amazonaws.com/vls-repository:openjdk11-base',
+                    'cpu': 256,
+                    'memory': 512,
+                    'memoryReservation': 256,
+                    'portMappings': [
+                        {
+                            'containerPort': 8081,
+                            'hostPort': 8081,
+                            'protocol': 'tcp'
+                        },
+                    ],
+                    'essential': True,
+                    'startTimeout': 10,
+                    'stopTimeout': 10,
+                    'user': '1001:1001',
+                    'workingDirectory': '/home',
+                    'disableNetworking': False,
+                    'privileged': False,
+                    'readonlyRootFilesystem': False,
+        
+                    'interactive': False,
+                    'pseudoTerminal': False,
+        
+                },
+            ],
+        
+            requiresCompatibilities=[
+                'FARGATE',
+            ],
+            cpu='256',
+            memory='512',
+            tags=[
+                {
+                    'key': 'env',
+                    'value': 'dev'
+                },
+            ],
+        )
